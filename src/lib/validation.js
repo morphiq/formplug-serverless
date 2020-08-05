ce9b148c0b58ea83eb445c4025133e76df49ea89
Patrick Menlove <patmenlove@gmail.com>
2020-08-05 17:32:55 +0100

    domains can have 7 characters

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,5 +1,5 @@
 module.exports.isWebsite = string => {
-  return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(string)
+  return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,7}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(string)
 }
 
 module.exports.isEmail = string => {
